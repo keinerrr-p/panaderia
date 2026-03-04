0207fd34eea20100116ab188258779d3bdbc5fd6
Nikorasu985 <alvaradonicolas347@gmail.com>
2026-03-04 16:45:47 -0500

    imagen y botones arreglados

--- obj/Debug/net10.0/AplicativoWebMVC.AssemblyInfo.cs
+++ obj/Debug/net10.0/AplicativoWebMVC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AplicativoWebMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba07ebd22e5b77edcfcd44d3c20b1b14343f0388")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ea3584ab9259675e725cac044887939dd438a84")]
 [assembly: System.Reflection.AssemblyProductAttribute("AplicativoWebMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AplicativoWebMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
